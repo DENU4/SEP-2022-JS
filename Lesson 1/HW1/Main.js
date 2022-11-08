c4e7f305d6375dcdc97c5e598b0782633d19afd4
DENU4 <denu4koluma@gmail.com>
2022-11-09 00:03:23 +0200

    final result

---
+++ Lesson 1/HW1/Main.js
@@ -0,0 +1,31 @@
+let hello = 'hello';
+console.log(hello);
+const str = 'owu';
+console.log(str);
+let com = 'com';
+console.log(com);
+let ua = 'ua';
+console.log(ua);
+let num = 1;
+console.log(num);
+let num1 = 10;
+console.log(num1);
+let num2 = -999;
+console.log(num2);
+let num3 = 123;
+console.log(num3);
+let bool1 = true;
+console.log(bool1);
+let bool2 = false;
+console.log(bool2);
+const PI = (3.14);
+console.log(PI);
+
+
+
+let firstName ='Denis';
+let middleName = 'Volodymyrovych';
+let lastName = 'Kolomiets';
+let result = firstName + ' ' + middleName + ' ' + lastName
+console.log(result);
+
